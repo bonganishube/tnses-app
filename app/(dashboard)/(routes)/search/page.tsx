8ab45abf4e5c3ee46c619c9819279310bd312a27
Bongani Shube <bonganishubeaz@gmail.com>
2024-12-21 20:19:05 +0200

    Finalise categories badges

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,9 +1,22 @@
+import { db } from '@/lib/db'
 import React from 'react'
+import { Categories } from './_components/categories';
 
-const page = () => {
+
+const SearchPage = async () => {
+  const categories = await db.category.findMany({
+      orderBy: {
+        name: "asc"
+      }
+  });
+
   return (
-    <div>page</div>
+    <div className="p-6">
+      <Categories
+        items={categories}
+      />
+    </div>
   )
 }
 
-export default page
+export default SearchPage
