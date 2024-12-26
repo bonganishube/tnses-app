b4b61bf7745851da980ec96510381d73a2a829b8
Bongani Shube <bonganishubeaz@gmail.com>
2024-12-26 18:29:10 +0200

    Resolve deployment issues

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: [
-            "utfs.io"
-        ]
-    }
-}
-
-module.exports = nextConfig
+      domains: ["utfs.io"],
+    },
+    experimental: {
+      missingSuspenseWithCSRBailout: false,
+    },
+  };
+  
+  module.exports = nextConfig;
+  
