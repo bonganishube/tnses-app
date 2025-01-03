751ba093afc9f8b091acb4fad092c52c3657501d
Bongani Shube <bonganishubeaz@gmail.com>
2025-01-03 19:09:05 +0200

    Add Ads logo on header

--- app/(course)/courses/[courseId]/_components/course-navbar.tsx
+++ app/(course)/courses/[courseId]/_components/course-navbar.tsx
@@ -1,37 +1,39 @@
-import SidebarRoutes from '@/app/(dashboard)/_components/sidebar-routes';
-import { Chapter, Course, UserProgress } from '@prisma/client';
-import React from 'react';
-import CourseMobileSidebar from './course-mobile-sidebar';
-import { UserButton } from '@clerk/nextjs';
+import SidebarRoutes from "@/app/(dashboard)/_components/sidebar-routes";
+import { Chapter, Course, UserProgress } from "@prisma/client";
+import React from "react";
+import CourseMobileSidebar from "./course-mobile-sidebar";
+import { UserButton } from "@clerk/nextjs";
+import Logo from "@/public/logo.png";
+import Image from "next/image";
 
 interface CourseNavbarProps {
-    course: Course & {
-        chapters: (Chapter & {
-            userProgress: UserProgress[] | null;
-        })[];
-    };
-    progressCount: number;
+  course: Course & {
+    chapters: (Chapter & {
+      userProgress: UserProgress[] | null;
+    })[];
+  };
+  progressCount: number;
 }
 
-const CourseNavbar = ({
-    course,
-    progressCount,
-}: CourseNavbarProps) => {
+const CourseNavbar = ({ course, progressCount }: CourseNavbarProps) => {
   return (
     <div className="p-4 h-12 w-full flex items-center place-content-between bg-white">
-        <div className="flex">
-            <CourseMobileSidebar
-                course={course}
-                progressCount={progressCount}
-            />
+      <div className="flex">
+        <CourseMobileSidebar course={course} progressCount={progressCount} />
+      </div>
+      <div className="flex flex-row items-center gap-2">
+        <div className="aspect-square size-8 justify-center rounded-lg text-sidebar-primary-foreground">
+          <Image src={Logo} alt="Team logo" className="rounded-[5px]" />
         </div>
-        <div className="flex mr-2">
-            <UserButton 
-                afterSignOutUrl="/"
-            />
-        </div>
+        <span className="truncate font-medium font-tertiary text-lg text-secondaryColor">
+          Ads
+        </span>
+      </div>
+      <div className="flex mr-2">
+        <UserButton afterSignOutUrl="/" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CourseNavbar
+export default CourseNavbar;
