cbd407a47cddae7bacfe402650b7b0a7fca34d02
Bongani Shube <bonganishubeaz@gmail.com>
2025-01-06 11:01:02 +0200

    Reduce margin right on sign-out button

--- app/(course)/courses/[courseId]/_components/course-sidebar.tsx
+++ app/(course)/courses/[courseId]/_components/course-sidebar.tsx
@@ -1,8 +1,8 @@
 import { db } from '@/lib/db';
 import { auth } from '@clerk/nextjs/server';
-import { Chapter, Course, UserProgress } from '@prisma/client'
+import { Chapter, Course, UserProgress } from '@prisma/client';
 import { redirect } from 'next/navigation';
-import React from 'react'
+import React from 'react';
 import CourseSidebarItem from './course-sidebar-item';
 import CourseProgress from '@/components/course-progress';
 import { SignOutButton } from '@clerk/nextjs';
@@ -25,7 +25,7 @@ const CourseSidebar = async ({
     const { userId } = await auth();
 
     if (!userId) {
-        return redirect("/")
+        return redirect("/");
     }
 
     const purchase = await db.purchase.findUnique({
@@ -37,43 +37,44 @@ const CourseSidebar = async ({
         }
     });
 
-  return (
-    <div className="h-screen border-r flex flex-col overflow-y-auto shadow-sm">
-        <div className="p-8 flex flex-col border-b">
-            <h1 className="font-semibold">
-                {course.title}
-            </h1>
-            {purchase && (
-                <div className="mt-10">
-                    <CourseProgress 
-                        variant="success"
-                        value={progressCount}
-                    />
-                </div>
-            )}
+    return (
+        <div className="h-screen border-r flex flex-col overflow-y-auto shadow-sm">
+            <div className="p-8 flex flex-col border-b">
+                <h1 className="font-semibold">
+                    {course.title}
+                </h1>
+                {purchase && (
+                    <div className="mt-10">
+                        <CourseProgress 
+                            variant="success"
+                            value={progressCount}
+                        />
+                    </div>
+                )}
+            </div>
+            <div className="flex-grow">
+                {course.chapters.map((chapter) => {
+                    return (
+                        <CourseSidebarItem 
+                            key={chapter.id}
+                            id={chapter.id}
+                            label={chapter.title}
+                            isCompleted={!!chapter.userProgress?.[0]?.isCompleted}
+                            courseId={course.id}
+                            isLocked={!chapter.isFree && !purchase}
+                        />
+                    );
+                })}
+            </div>
+            {/* Button at the bottom */}
+            <div className="p-2 mt-auto">
+                <Button size="sm" variant="outline" className="w-auto">
+                    <LogOut className="h-4 w-4 mr-2" />
+                    <SignOutButton />
+                </Button>
+            </div>
         </div>
-        <div>
-            {course.chapters.map((chapter) => {
-                return (
-                    <CourseSidebarItem 
-                        key={chapter.id}
-                        id={chapter.id}
-                        label={chapter.title}
-                        isCompleted={!!chapter.userProgress?.[0]?.isCompleted}
-                        courseId={course.id}
-                        isLocked={!chapter.isFree && !purchase}
-                    />
-                );
-            })}
-        </div>
-        <div className="inset-y-full relative p-2">
-            <Button size="sm" variant="outline" className="w-auto">
-                <LogOut className="h-4 w-4 mr-2" />
-                <SignOutButton />
-            </Button>
-        </div>
-    </div>
-  )
+    );
 }
 
-export default CourseSidebar
+export default CourseSidebar;
